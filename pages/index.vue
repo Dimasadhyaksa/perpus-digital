f9d3a5e3402353f6359dce2c43c0640766664c8b
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-03-14 10:22:12 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container-fluid">
+        <div class="row my-5">
+            <div class="col-lg-6">
+                <nuxt-link to="/pengunjung/tambah/">
+                    <div class="card-body">
+                        <h2>pengunjung</h2>
+                    </div>
+                </nuxt-link>
+            </div>
+        </div>
+    </div>
+</template>
